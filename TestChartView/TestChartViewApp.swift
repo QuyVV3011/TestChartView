caa87083b632c9f556b4b186ad230951fff9273f
QuyVV3011 <QuyVV@apero.vn>
2025-05-29 08:42:27 +0700

    Initial Commit

---
+++ TestChartView/TestChartViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestChartViewApp.swift
+//  TestChartView
+//
+//  Created by admin on 29/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TestChartViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestChartViewTests/TestChartViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestChartViewTests.swift
+//  TestChartViewTests
+//
+//  Created by admin on 29/5/25.
+//
+
+import Testing
+
+struct TestChartViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
